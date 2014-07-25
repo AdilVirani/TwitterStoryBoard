f385f52246a34d26178de8ceecab8538e8d8589c
AdilVirani <adisv9997@gmail.com>
2014-07-25 00:02:06 -0500

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  XHTwitterPaggingViewerStoryboradExample
+//
+//  Created by Adil Virani on 6/19/14.
+//  Copyright (c) 2014 Adil Virani. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
