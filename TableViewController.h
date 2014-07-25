c91c10892a026df0127df1a5ed70ab66ce597cd6
AdilVirani <adisv9997@gmail.com>
2014-07-25 00:04:32 -0500

    Create TableViewController.h

---
+++ TableViewController.h
@@ -0,0 +1,17 @@
+//
+//  XHTableViewController.h
+//  TwitterPagging
+//
+//  Created by Adil Virani on 6/19/14.
+//  Copyright (c) 2014 Adil Virani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XHBaseTableViewController.h"
+
+@interface XHTableViewController : XHBaseTableViewController
+
+@property (nonatomic, assign) BOOL showPushDetail;
+
+@end
