b7f475231267ffc2f453819d998eae5aa3749380
AdilVirani <adisv9997@gmail.com>
2014-07-25 00:03:53 -0500

    Create Foundation.h

---
+++ Foundation.h
@@ -0,0 +1,18 @@
+//
+//  XHFoundationCommon.h
+//  MessageDisplayExample
+//
+//  Created by Adil Virani on 6/19/14.
+//  Copyright (c) 2014 Adil Virani. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface XHFoundationCommon : NSObject
+
++ (BOOL)isIOS7;
+
++ (CGFloat)getAdapterHeight;
+
+@end
